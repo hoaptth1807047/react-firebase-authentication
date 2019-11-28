ec30f3bdebf9742866758227fba726c720fa44e0
thanh hoa <hoaptth1807047@fpt.edu.vn>
2019-11-28 09:01:45 +0700

    delete

---
+++ src/components/Account/create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreatePage extends React.Component{
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CreatePage;
